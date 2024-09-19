9315ca38f89a35f5aee8f397306d9d736ef3aefd
Ken Wan <kenwan006@gmail.com>
2024-09-18 18:07:47 -0700

    update localthread

--- multi_threading/src/main/java/ThreadPoolDemo.java
+++ multi_threading/src/main/java/ThreadPoolDemo.java
@@ -1,2 +1,23 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolDemo {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3); //create a thread pool of size 3
+
+        //let's creat 10 threads, and see how thread pool manage them
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(200);
+                    System.out.println(Thread.currentThread().getName() + " is running.");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            executorService.execute(thread);
+        }
+        executorService.shutdown();
+    }
 }
